d016b91edef6b5a73151c1fb438ebbca84eb605a
Terrance Howard <heyterrance@gmail.com>
2015-07-03 13:15:57 -0500

    Adds fixed_string.

---
+++ include/ax/fixed_string.h
@@ -0,0 +1,146 @@
+// Fixed length string
+//
+
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <ostream>
+
+namespace ax {
+
+template<std::size_t Capacity, typename CharT = char>
+class fixed_string
+{
+public:
+    using value_type = CharT;
+    using iterator = CharT*;
+    using const_iterator = const CharT*;
+    using size_type = std::size_t;
+
+public:
+    fixed_string() = default;
+
+    constexpr fixed_string(const char* s)
+    {
+        size_type idx = 0;
+        while (idx != Capacity and *s != '\0') {
+            data_[idx++] = *(s++);
+        }
+    }
+
+    template<std::size_t N>
+    constexpr fixed_string(const char (&s)[N])
+    {
+        static_assert(
+                N >= Capacity,
+                "Cannot instantiate with string larger than Capacity.");
+        for (size_type i = 0; i != N; ++i)
+            data_[i] = s[i];
+    }
+
+    //
+    // Element access
+    //
+
+    constexpr
+    CharT& operator[](size_type pos)
+    {
+        return data_[pos];
+    }
+
+    constexpr
+    const CharT& operator[](size_type pos) const
+    {
+        return data_[pos];
+    }
+
+    constexpr       CharT& front()          { return data_[0]; }
+    constexpr const CharT& front() const    { return data_[0]; }
+    constexpr       CharT& back()           { return data_[length()]; }
+    constexpr const CharT& back() const     { return data_[length()]; }
+    constexpr const std::basic_string<CharT> str() const
+    {
+        return std::basic_string<CharT>(data_, length());
+    }
+    constexpr const CharT* c_str() const    { return data_; }
+    constexpr const CharT* data() const     { return c_str(); }
+
+    //
+    // Iterators
+    //
+
+    constexpr iterator          begin()         { return data_; }
+    constexpr const_iterator    begin() const   { return data_; }
+    constexpr const_iterator    cbegin() const  { return data_; }
+    constexpr iterator          end()           { return &back(); }
+    constexpr const_iterator    end() const     { return &back(); }
+    constexpr const_iterator    cend() const    { return &back(); }
+
+    //
+    // Capacity
+    //
+
+    constexpr
+    size_type length() const
+    {
+        size_type idx = 0;
+        while (idx != Capacity and data_[idx] != '\0') ++idx;
+        return idx;
+    }
+    constexpr size_type size() const { return length(); }
+    constexpr bool      empty() const { return data_[0] == '\0'; }
+    static
+    constexpr size_type capacity() { return Capacity; }
+    static
+    constexpr size_type max_size() { return Capacity; }
+
+    //
+    // Operations
+    //
+
+    void clear()
+    {
+        data_[0] = '\0';
+    }
+
+    fixed_string& insert(size_type index, size_type count, CharT ch)
+    {
+        for (size_type i = index; i < Capacity and count != 0; ++i, --count) {
+            data_[i] = ch;
+        }
+        return *this;
+    }
+
+    fixed_string& insert(size_type index, const CharT* s)
+    {
+        assert(Capacity > index);
+        std::strncpy(&data_[index], s, Capacity - index);
+        return *this;
+    }
+
+    template<std::size_t Cap>
+    fixed_string& append(const fixed_string<Cap, CharT>& s)
+    {
+        return append(s.c_str());
+    }
+
+    fixed_string& append(const CharT* s)
+    {
+        return insert(length(), s);
+    }
+
+    friend std::ostream& operator<<(std::ostream&, const fixed_string&);
+
+private:
+    CharT data_[Capacity] = { 0 };
+};
+
+template<std::size_t C, typename T>
+inline
+std::ostream& operator<<(std::ostream& s, const fixed_string<C, T>& src)
+{
+    return s << src.data_;
+}
+} // namespace ax
---
+++ test/fixed_string.cpp
@@ -0,0 +1,47 @@
+
+#include <Catch/catch.hpp>
+
+#include <ax/fixed_string.h>
+
+using S4 = ax::fixed_string<4, char>;
+using S8 = ax::fixed_string<8, char>;
+using S16 = ax::fixed_string<16, char>;
+using S21 = ax::fixed_string<21, char>;
+
+TEST_CASE("fixed string constexpr", "[fixed_string]")
+{
+    constexpr S4 s("Hi");
+    static_assert(s.capacity() == 4, "Incorrect capacity.");
+    static_assert(s.length() == 2, "Incorrect length.");
+    static_assert(s.size() == s.length(), "Incorrect size.");
+    static_assert(s.end() - s.begin() == s.size(), "Bad iterators.");
+}
+
+SCENARIO("fixed string insertions", "[fixed_string]")
+{
+    GIVEN("an empty fixed_string") {
+        S16 s;
+        REQUIRE(s.empty());
+
+        WHEN("a small string is appended") {
+            s.append("a");
+            THEN("the string is added") {
+                REQUIRE(s.length() == 1);
+            }
+        }
+        WHEN("two small strings are appened") {
+            s.append("a").append("b");
+            THEN("the strings are in the correct order") {
+                REQUIRE(s.length() == 2);
+                REQUIRE(s.str() == "ab");
+            }
+        }
+        WHEN("another fixed string is appened") {
+            s.append("a").append(S8("bcd"));
+            THEN("all the contents are appened") {
+                REQUIRE(s.length() == 4);
+                REQUIRE(s.str() == "abcd");
+            }
+        }
+    }
+}
