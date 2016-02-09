0c5aa5881cacd43529f94e98a7e0a8bb0d6472f4
Terrance Howard <heyterrance@gmail.com>
2016-02-08 18:05:24 -0600

    Fixes multipart license

--- test/multipart.cpp
+++ test/multipart.cpp
@@ -1,18 +1,18 @@
 /*
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                    Version 2, December 2004
- *
- * Copyright (C) 2015 Howard, Terrance <heyterrance@gmail.com>
+ * Copyright 2016 Howard, Terrance <heyterrance@gmail.com>
  * Author: Howard, Terrance <heyterrance@gmail.com>
  *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <Catch/catch.hpp>
